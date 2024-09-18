e7b0a98fa089bfd80db0d4af17058e03cd0d371d
Vladimir Vassilev <vladimir@lightside-instruments.com>
2024-09-18 15:11:37 +0200

    yuma123-netconfd-module-lsi-ivi-scope-test-1.5-17

--- example/find-resonance.m
+++ example/find-resonance.m
@@ -1,3 +1,14 @@
+% Method for system characterization based on frequency
+% sweep probing signal and calculation of pairs of orthogonal
+% signal components of the returned signal.
+%
+% The calculation of the orthogonal signal components can be implemented by
+% calculation of a dot product of the samples representing short periods
+% of the signal sent and the signal received back and a dot product of the
+% orthogonal signal to the one sent and the signal received back.
+%
+% The orthogonal signals can be cosine and sine frequency sweeps.
+
 pkg load signal
 
 generate_chirp
@@ -17,11 +28,33 @@ offset=[0:delta:length(chirp1)-delta]';
 for i = 1:length(offset)
   dotproduct_array(i)=dot(signal_wo_delay(1+offset(i):offset(i)+delta),chirp1(1+offset(i):offset(i)+delta));
 end
-format long
-[value,index]=max(dotproduct_array)
-resonant_frequency = start_freq + (stop_freq-start_freq)*index/length(dotproduct_array)
-plot([start_freq:(stop_freq-start_freq)/length(dotproduct_array):stop_freq-(stop_freq-start_freq)/length(dotproduct_array)],dotproduct_array)
+
+signal = signal_wo_delay-mean(signal_wo_delay);
+chirp_cos = chirp1;
+chirp_sin = [chirp1(1+8:end); zeros(8,1)];
+delta=1000
+
+offset = [0:delta:length(signal)-1]
+for i = 1:length(offset)
+  dotproduct_chirp_cos(i)=dot(signal(1+offset(i):offset(i)+delta),chirp_cos(1+offset(i):offset(i)+delta));                                                                                   
+  dotproduct_chirp_sin(i)=dot(signal(1+offset(i):offset(i)+delta),chirp_sin(1+offset(i):offset(i)+delta));
+end
+result=complex(dotproduct_chirp_cos,dotproduct_chirp_sin)
+
+figure(1)
+magnitude = abs(result);
+plot([start_freq:(stop_freq-start_freq)/length(magnitude):stop_freq-(stop_freq-start_freq)/length(magnitude)],magnitude);
+[value,index]=max(magnitude)
+resonant_peak_frequency = start_freq + (stop_freq-start_freq)*(index-1)/length(magnitude)
 hold on
-plot([resonant_frequency resonant_frequency],[0 value])
+plot([resonant_peak_frequency resonant_peak_frequency],[min(magnitude) max(magnitude)])
 hold on
-title(['dotproduct max index = ' num2str(resonant_frequency)])
+title(['Magnitude (max_index) = ' num2str(resonant_peak_frequency)])
+
+figure(2)
+plot(angle(result))
+title('Angle')
+
+figure(3)
+plot(signal)
+title('Raw signal')
--- example/generate_chirp.m
+++ example/generate_chirp.m
@@ -1,13 +1,12 @@
 pkg load signal
 
-#start_freq=1550
-start_freq=1598
-#stop_freq=1700
-stop_freq=1600
+start_freq=1595
+#start_freq=1597
+stop_freq=1610
+#stop_freq=1602
 samples=480000
 sample_freq=48000
 
 #audiowrite('signal-out.wav',[0.05*chirp([0:1/sample_freq:10-1/sample_freq],start_freq,10,stop_freq,'linear')',zeros(1,samples)'],sample_freq);
-chirp1 = chirp([0:1/sample_freq:10-1/sample_freq],stop_freq,10,start_freq,'linear');
+chirp1 = chirp([0:1/sample_freq:10-1/sample_freq],start_freq,10,stop_freq,'linear');
 audiowrite('signal-out.wav',[0.05*chirp1',zeros(1,samples)'],sample_freq);
-
