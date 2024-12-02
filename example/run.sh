e6056a071afb28d0e950f8f5992ba7628735b8f5
Vladimir Vassilev <vladimir@lightside-instruments.com>
2024-12-02 17:50:05 +0100

    yuma123-netconfd-module-lsi-ivi-scope-test-1.5-26

--- example/run.sh
+++ example/run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash -xe
 export directory=`date +%Y%m%d%H%M%S`
 mkdir report/${directory}
+git log -1 > git.txt
+git diff >> report/${directory}/git.txt
 #python3 test-w-generator.py --config=networks.xml --scope-name=scope0 --generator-name=generator0 --generator-amplitude=0.50 --scope-channel-name="hw:CODEC,0" --scope-channel-range=4.0 --scope-channel-parameters="-c 2 -r 48000 -f S16_LE" --generator-channel-name="default" --samples=960000 --sample-rate=48000 --scope-trigger-source=ch3 --scope-trigger-level=0.4 --scope-trigger-slope=positive > report/${directory}/log.txt
 #python3 test-w-generator.py --config=networks.xml --scope-name=scope0 --generator-name=generator0 --generator-amplitude=10.00 --scope-channel-name="ch1" --scope-channel-range=4.0 --generator-channel-name="default" --samples=64000 --sample-rate=40000 --scope-trigger-source=ch2 --scope-trigger-level=1.4 --scope-trigger-slope=negative --scope-channel-parameters=" " --start-frequency=1595.00 --stop-frequency=1610.00 --sweep-time=1.0
-python3 test-w-generator.py --config=networks.xml --scope-name=scope0 --generator-name=generator0 --generator-amplitude=10.00 --scope-channel-name="ch1" --scope-channel-range=4.0 --generator-channel-name="default" --samples=64000 --sample-rate=40000 --scope-trigger-source=ch2 --scope-trigger-level=1.4 --scope-trigger-slope=negative --scope-channel-parameters=" " --start-frequency=1580.00 --stop-frequency=1610.00 --sweep-time=1.0
+python3 test-w-generator.py --config=networks.xml --scope-name=scope0 --generator-name=generator0 --generator-amplitude=10.00 --scope-channel-name="ch1" --scope-channel-range=4.0 --generator-channel-name="default" --samples=64000 --sample-rate=40000 --scope-trigger-source=ch2 --scope-trigger-level=1.4 --scope-trigger-slope=negative --scope-channel-parameters=" " --start-frequency=1580.00 --stop-frequency=1610.00 --sweep-time=1.0 | tee report/${directory}/log.txt
 cp signal.wav report/${directory}
-# octave-cli find-resonance.m | tee report/${directory}/find_resonance.log
-octave-cli --persist find-resonance.m
+octave-cli find-resonance.m | tee report/${directory}/find_resonance.log
+# octave-cli --persist find-resonance.m
 
