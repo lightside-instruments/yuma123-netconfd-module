2d2e2b0f6ffacb571b41ffdef4f35fefe1d00df2
Vladimir Vassilev <vladimir@lightside-instruments.com>
2025-01-07 12:21:54 +0100

    yuma123-netconfd-module-lsi-ivi-scope-test-1.5-28

--- mod.c
+++ mod.c
@@ -286,7 +286,7 @@ static int update_config(val_value_t* config_cur_val, val_value_t* config_new_va
            free(range_str);
         }
 
-        sprintf(rm_cmd_buf, "rm /tmp/lsi-ivi-scope-%s-signal.wav", VAL_STRING(name_val));
+        sprintf(rm_cmd_buf, "rm tmp/lsi-ivi-scope-%s-signal.wav", VAL_STRING(name_val));
         printf("Calling %s\n",rm_cmd_buf);
         system(rm_cmd_buf);
     }
@@ -294,7 +294,7 @@ static int update_config(val_value_t* config_cur_val, val_value_t* config_new_va
     sprintf(cmd_buf, "%s %s", "lsi-ivi-scope-acquisition-start", params_buf);
     printf("Calling: %s\n", cmd_buf);
     system(cmd_buf);
-    sprintf(cmd_buf, "%s %s 1>/tmp/acquisition.stdout 2>/tmp/acquisition.stderr", "lsi-ivi-scope-acquisition-complete", params_buf);
+    sprintf(cmd_buf, "%s %s 1>tmp/acquisition.stdout 2>tmp/acquisition.stderr", "lsi-ivi-scope-acquisition-complete", params_buf);
     printf("Calling: %s\n", cmd_buf);
 
     {
