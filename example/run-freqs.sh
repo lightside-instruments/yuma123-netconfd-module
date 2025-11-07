fd96a7fea891abe5a536100845abae01db4cf44b
Vladimir Vassilev <vladimir@lightside-instruments.com>
2025-11-07 12:49:40 +0100

    yuma123-netconfd-module-lsi-ivi-scope-test-1.5-40

--- example/run-freqs.sh
+++ example/run-freqs.sh
@@ -2,7 +2,7 @@
 mkdir -p results
 while read p; do
   echo "$p"
-  cp results/signal-ch1-${p}.wav signal-ch1.wav 
-  cp results/signal-ch2-${p}.wav signal-ch2.wav
-  md5sum signal-ch1.wav signal-ch2.wav
+  python3 ./test-w-generator-standard-function.py --config=networks.xml --scope-name=scope-rigol --generator-name=generator0 --scope-channel-name=ch1,ch2 --scope-channel-range="4.0,4.0" --scope-channel-parameters="-,-" --generator-channel="ch1" --generator-waveform-type=sine --generator-amplitude=1.0 --generator-dc-offset=0 --samples=600000 --sample-rate=100000 --generator-frequency=$p --scope-trigger-source=ch2 --scope-trigger-level=0.1 --scope-trigger-slope=positive
+  mv signal-ch1.wav results/signal-ch1-${p}.wav
+  mv signal-ch2.wav results/signal-ch2-${p}.wav
 done <freqs.txt
